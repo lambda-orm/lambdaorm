5ff66a8bd33d8a0b1d40dece0c8875b16f10531a
FlavioLionelRita <flaviolrital@hotmail.com>
2021-05-30 16:12:55 +0200

    add describe test

--- test/test.js
+++ test/test.js
@@ -30,18 +30,20 @@ ORDER BY category, name desc
         });
     });
 
-    describe('select from join whrere order by', function() {        
+    describe('select from joins whrere order by', function() {        
         let expression =
         `OrderDetail.filter(p=> between(p.order.shippedDate,'19970101','19971231') )                 
                     .map(p=> {category: p.product.category.name,product:p.product.name,unitPrice:p.unitPrice,quantity:p.quantity})
                     .sort(p=> [p.category,p.product])
         `;
         let expected =
-`SELECT c.CategoryName AS category, p.ProductName AS name, p.QuantityPerUnit AS quantity, p.UnitsInStock AS inStock 
-FROM Products p 
+`SELECT c.CategoryName AS category, p.ProductName AS product, o.UnitPrice AS unitPrice, o.Quantity AS quantity 
+FROM OrderDetails o 
+INNER JOIN Orders o1 ON o1.OrderID = o.OrderID
+INNER JOIN Products p ON p.ProductID = o.ProductID
 INNER JOIN Categories c ON c.CategoryID = p.CategoryID
-WHERE p.Discontinued <> 0 
-ORDER BY category, name desc 
+WHERE o1.ShippedDate BETWEEN '19970101' AND '19971231' 
+ORDER BY category, product 
 `;
         it(expression, function() {
             let sentence = exp.sentence(expression,'northwind','orm','oracle');
