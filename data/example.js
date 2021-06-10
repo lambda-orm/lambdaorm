d020dff52b779d76e2563cd9276283034bae7826
Flavio Lionel Rita <flaviolrita@hotmail.com>
2021-06-10 12:24:23 +0200

    bkp config schema

--- data/example.js
+++ data/example.js
@@ -7,8 +7,8 @@
 //https://www.dabapps.com/blog/django-models-and-encapsulation/
 
 
-orm.run(()=> Order.filter(p=> p.id = id)
-                   .include(OrderDetail.filter(p => p.enable).map(p=> p)
-                           .include(Product).map(p=> p.name))
-                   .include(Customer.map(p=> p.name))         
-        ,{id:1} );
+orm.run(()=> Order.filter(p=> p.id == id)
+                  .include(OrderDetail.filter(p => p.enable == true)
+                          .include(Product).map(p=> p.name))
+                  .include(Customer.map(p=> p.name))         
+        ,{id:1});
