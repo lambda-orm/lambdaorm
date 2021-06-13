2c4e05aa6a7002c06926735fd438eb23d99774df
FlavioLionelRita <flaviolrital@hotmail.com>
2021-06-13 15:31:01 +0200

    solve mapping entoty error

--- src/manager/schema.ts
+++ src/manager/schema.ts
@@ -38,7 +38,7 @@ export default class SchemaManager
         target.name = source.name;
         for(const p in source.entities){
             let sourceEntity = source.entities[p];
-            let targetEntity:any = { property:{},relation:{}};    
+            let targetEntity:any = {name: sourceEntity.name,mapping:sourceEntity.mapping,property:{},relation:{}};    
             for(const q in sourceEntity.properties){
                 let sourceProperty = sourceEntity.properties[q];
                 targetEntity.property[sourceProperty.name] = sourceProperty;
