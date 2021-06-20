dfcc04358652bb24f29e1ba7bdd4452accbd0e44
FlavioLionelRita <flaviolrital@hotmail.com>
2021-06-20 11:55:16 +0200

    begin numeric functions and resolve bug in parse float

--- test/debug.js
+++ test/debug.js
@@ -18,15 +18,48 @@ orm.addConnection(cnx);
 
 expression =
 ` 
-Orders.filter(p=>p.id==id).include(p => p.customer)
+Products.filter(p=>p.id == id).map(p=>{name:p.name,source:0.50,result:atan2(0.25,1)})
 `;
 //plan 
-result = orm.expression(expression).compile('sql','mysql','northwind').serialize();
-console.log(JSON.stringify(result));
-//ejecucion
-// let context = {id:10582}
-// result = await orm.expression(expression).run(context,'northwind');
+// result = orm.expression(expression).compile('sql','mysql','northwind').serialize();
 // console.log(JSON.stringify(result));
+//ejecucion
+let context = {id:1}
+result = await orm.expression(expression).run(context,'northwind');
+console.log(JSON.stringify(result));
+
+
+// functions
+//  numeric:
+
+// Products.filter(p=>p.id == id ).map(p=> {name:p.name,source:p.price ,result:abs(p.price)} )
+
+// ceil: 'CEIL({0})'
+// cos: 'COS({0})'
+// cosh: 'COSH({0})'
+// exp: 'EXP({0})'
+// floor: 'FLOOR({0})'
+// ln: 'LN({0})'
+// log: 'LOG({0},{1})'
+// nanvl: 'NANVL({0},{1})'
+// remainder: 'REMAINDER({0},{1})'
+// round: 'ROUND({0})'
+// sign: 'SIGN({0})'
+// sin: 'SIN({0})'
+// sinh: 'SINH({0})'
+// tan: 'TAN({0})'
+// tanh: 'TANH({0})'
+// trunc: 'TRUNC({0})'
+
+
+//queries
+// Products.filter(p=> p.discontinued != false )                 
+//                  .map(p=> ({category:p.category.name,name:p.name,quantity:p.quantity,inStock:p.inStock}) )
+//                  .sort(p=> [p.category,desc(p.name)])
+// OrderDetails.filter(p=> between(p.order.shippedDate,'19970101','19971231') )                 
+//                      .map(p=> ({category: p.product.category.name,product:p.product.name,unitPrice:p.unitPrice,quantity:p.quantity}))
+//                      .sort(p=> [p.category,p.product])       
+// OrderDetails.map(p=> ({order: p.orderId,subTotal:sum((p.unitPrice*p.quantity*(1-p.discount/100))*100) }))
 
 //includes
 //Orders.filter(p=>p.id==id).include(p => [p.details.include(q=>q.product).map(p=>({quantity:p.quantity,unitPrice:p.unitPrice,productId:p.productId})),p.customer])
@@ -37,13 +70,6 @@ console.log(JSON.stringify(result));
 //Orders.filter(p=>p.id==id).include(p => p.details)
 //Orders.filter(p=>p.id==id).include(p => p.customer)
 
-//queries
-// Products.filter(p=> p.discontinued != false )                 
-//                  .map(p=> ({category:p.category.name,name:p.name,quantity:p.quantity,inStock:p.inStock}) )
-//                  .sort(p=> [p.category,desc(p.name)])
-// OrderDetails.filter(p=> between(p.order.shippedDate,'19970101','19971231') )                 
-//                      .map(p=> ({category: p.product.category.name,product:p.product.name,unitPrice:p.unitPrice,quantity:p.quantity}))
-//                      .sort(p=> [p.category,p.product])       
-// OrderDetails.map(p=> ({order: p.orderId,subTotal:sum((p.unitPrice*p.quantity*(1-p.discount/100))*100) }))
+
 
 })();
