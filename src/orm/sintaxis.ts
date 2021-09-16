68d46539e9280507da219f81e943978dc7a4824c
Flavio Lionel Rita <frita@beesion.com>
2021-09-16 05:28:48 +0200

    working with eslint

--- src/orm/sintaxis.ts
+++ src/orm/sintaxis.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/ban-types */
-// // export declare function s<T>(someObject: T | null | undefined, defaultValue?: T | null | undefined) : T
 export interface PageClauses<T> {
 	/**  */
 	page(page:number, records:number):void
