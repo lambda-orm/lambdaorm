a48e888ebcf0f6db0755f3daf90f1455e44fbd8c
FlavioLionelRita <flaviolrital@hotmail.com>
2021-09-18 17:25:10 +0200

    begin work grunt in linux

--- src/test/Gruntfile.js
+++ src/test/Gruntfile.js
@@ -4,21 +4,16 @@ module.exports = function (grunt) {
 	// Project configuration.
 
 	grunt.initConfig({
-		mysql_query: {
-			createDbSource: {
-				host: '0.0.0.0',
-				user: 'root',
-				pass: 'root',
-				database: '',
-				queries: [{ file: 'src/test/db/northwind-mysql.sql' }]
-			}
+		exec: {
+			start_dbs: { cmd: 'start.sh', cwd: 'db' }
 		}
 	})
 
 	// Load the plugins
 	grunt.loadNpmTasks('grunt-contrib-uglify')
-	grunt.loadNpmTasks('grunt-mysql-query')
+	grunt.loadNpmTasks('grunt-exec')
+
 
 	// Default task(s).
-	grunt.registerTask('default', ['mysql_query:createDbSource'])
+	grunt.registerTask('default', ['exec:start_dbs'])
 }
