89c766de474a5b46e43dbdec9aaaf775703436c6
Flavio Lionel Rita <frita@beesion.com>
2021-09-26 15:37:25 +0200

    solve types in mysql

--- src/orm/connection/dialects/mysql.ts
+++ src/orm/connection/dialects/mysql.ts
@@ -6,33 +6,33 @@ import { Parameter } from '../../model'
 import { timeStamp } from 'console'
 
 const DECIMAL = 0
-const TINY = 1
-const SHORT = 2
+// const TINY = 1
+// const SHORT = 2
 const LONG = 3
 const FLOAT = 4
 const DOUBLE = 5
-const NULL = 6
+// const NULL = 6
 const TIMESTAMP = 7
 const LONGLONG = 8
 const INT24 = 9
 const DATE = 10
 const TIME = 11
 const DATETIME = 12
-const YEAR = 13
+// const YEAR = 13
 const NEWDATE = 14
-const VARCHAR = 15
+// const VARCHAR = 15
 const BIT = 16
-const JSON = 245
+// const JSON = 245
 const NEWDECIMAL = 246
-const ENUM = 247
-const SET = 248
-const TINY_BLOB = 249
-const MEDIUM_BLOB = 250
-const LONG_BLOB = 251
-const BLOB = 252
-const VAR_STRING = 253
-const STRING = 254
-const GEOMETRY = 255
+// const ENUM = 247
+// const SET = 248
+// const TINY_BLOB = 249
+// const MEDIUM_BLOB = 250
+// const LONG_BLOB = 251
+// const BLOB = 252
+// const VAR_STRING = 253
+// const STRING = 254
+// const GEOMETRY = 255
 
 export class MySqlConnectionPool extends ConnectionPool {
 	private static mysql:any
