39ed523715aab20d3925f547d44b3d011125278b
Flavio Lionel Rita <frita@beesion.com>
2021-10-24 11:28:34 +0200

    refactorize language

--- src/orm/connection/executor.ts
+++ src/orm/connection/executor.ts
@@ -44,19 +44,6 @@ export class Executor {
 		return result
 	}
 
-	// protected async _execute (query:Query, context:Context, metadata:DialectMetadata, executor:Executor):Promise<any> {
-	// let result:any
-	// switch (query.name) {
-	// case 'select': result = await this.select(query, context, metadata, executor); break
-	// case 'insert': result = await this.insert(query, context, metadata, executor); break
-	// case 'update': result = await this.update(query, context, metadata, executor); break
-	// case 'delete': result = await this.delete(query, context, metadata, executor); break
-	// case 'bulkInsert': result = await this.bulkInsert(query, context, metadata, executor); break
-	// default: throw new Error(`sentence ${query.name} not implemented`)
-	// }
-	// return result
-	// }
-
 	public async execute (query:Query):Promise<any> {
 		const connection = await this.connectionManager.acquire(this.connectionName)
 		const result = await connection.execute(query)
