80d7cd0e72445af02aa690081b72cbb134c3b641
FlavioLionelRita <flaviolrital@hotmail.com>
2022-07-01 14:18:58 +0200

    bugfix db.sh script initialized databases

--- src/dev/db/db.sh
+++ src/dev/db/db.sh
@@ -3,14 +3,44 @@
 #    ./db.sh up
 #    ./db.sh down
 
+wait-dbs(){
+	wait-until-healthy 'lambdaORM-Source'
+	wait-until-healthy 'lambdaORM-MySQL-57'
+	wait-until-healthy 'lambdaORM-Postgres-10'
+	wait-until-healthy 'lambdaORM-Oracle-19' 600
+	# wait-until-healthy 'lambdaORM-SqlServer'	
+	# wait-until-healthy 'lambdaORM-MongoDB'
+	# wait-until-healthy 'lambdaORM-MariaDB-103'		
+}
+
 wait-until-healthy(){
-	./wait-until-healthy.sh lambdaORM-Source
-	./wait-until-healthy.sh lambdaORM-MySQL-57
-	./wait-until-healthy.sh lambdaORM-Postgres-10
-	./wait-until-healthy.sh lambdaORM-MariaDB-103
-	./wait-until-healthy.sh lambdaORM-MongoDB	
-	./wait-until-healthy.sh lambdaORM-SqlServer
-	./wait-until-healthy.sh lambdaORM-Oracle-19	
+	if [ ! -z "$1" ]; then
+		IMAGE=$1
+	else
+		>&2 echo "Please provide the container name or hash"
+		exit 1
+	fi
+
+	# set waiting time 
+	if [ ! -z "$2" ]; then
+			TIME=($2 / 50)
+	else
+			TIME=1.2   
+	fi
+
+	for _ in {1..50}
+	do
+		state=$(docker inspect -f '{{ .State.Health.Status }}' ${IMAGE} 2>&1)
+		return_code=$?
+		if [ ${return_code} -eq 0 ] && [ "$state" == "healthy" ]; then
+			echo "${IMAGE} is healthy!"
+			return
+		fi
+		sleep ${TIME}
+	done
+
+	>&2 echo "Timeout, exceeded when waiting for container to be healthy: ${IMAGE}"
+	exit 1
 }
 
 create_db_users(){
@@ -27,18 +57,14 @@ create_db_users(){
 
   # https://community.bmc.com/s/article/Remedy-Server-Error-ORA-65096-invalid-common-user-or-role-name-installing-ARS-9-1-on-Oracle-12c#:~:text=The%20error%20ORA%2D65096%3A%20invalid,name%20is%20a%20Oracle%20error.&text=Cause%3A%20An%20attempt%20was%20made,for%20common%20users%20or%20roles.
   # TODO: solve "Error executing child process: Error: Process exited with code 127"
-	docker exec -it lambdaORM-Oracle-19 sqlplus system/password@ORCLCDB
-  conn sys/password as sysdba; 
-	alter session set "_ORACLE_SCRIPT"=true;
-	CREATE TABLESPACE northwind DATAFILE 'northwind.dat' SIZE 100M AUTOEXTEND ON;
-	CREATE USER northwind IDENTIFIED BY northwind DEFAULT TABLESPACE northwind QUOTA UNLIMITED ON northwind;
-  GRANT ALL PRIVILEGES TO northwind;
-	exit; 
+
+	docker exec -it lambdaORM-Oracle-19 sqlplus system/password @/home/oracle/setup/custom_scripts/startup.sql
+  
 }
 
 up(){
 	docker-compose up -d
-	wait-until-healthy
+	wait-dbs
 	create_db_users
 	echo "INFO: Databases instances is ready for tests."
 }
@@ -48,6 +74,7 @@ down(){
 	docker-compose down --remove-orphans
 	sudo chmod 755 ./volume/*
 	sudo rm -fR ./volume/*
+	docker volume rm db_oradata	
 	echo "INFO: stopped Databases (if it was running)."
 }
 
