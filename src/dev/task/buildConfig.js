33bb871fe0e675ee00ecf2f26f36571dc35c28f3
FlavioLionelRita <flaviolrital@hotmail.com>
2023-03-22 16:06:23 +0100

    update use config extends

--- src/dev/task/buildConfig.js
+++ src/dev/task/buildConfig.js
@@ -1,8 +1,8 @@
 /* eslint-disable space-before-function-paren */
-const ConfigExtends = require('config-extends')
+import { configExtends } from 'config-extends'
 exports.apply = async function apply(callback) {
-	await ConfigExtends.apply('src/dev/config/SQL', 'src/lib/language/SQL/config.json')
-	await ConfigExtends.apply('src/dev/config/NoSQL', 'src/lib/language/NoSQL/config.json')
+	await configExtends.apply('src/dev/config/SQL', 'src/lib/language/SQL/config.json')
+	await configExtends.apply('src/dev/config/NoSQL', 'src/lib/language/NoSQL/config.json')
 	callback()
 }
 // apply(path.join(function () { console.log('end')})
