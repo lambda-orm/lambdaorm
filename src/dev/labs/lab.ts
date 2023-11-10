f496198b6f9871e6eeaa1142bac51e1031208bf7
FlavioLionelRita <flaviolrital@hotmail.com>
2023-11-10 03:30:07 +0100

    fix(security): update dependences
    #176

--- src/dev/labs/lab.ts
+++ src/dev/labs/lab.ts
@@ -6,12 +6,15 @@ export async function apply (callback: any) {
 		await orm.init('./config/northwind.yaml')
 		const options = {stage:'MySQL'}	
 		
-		const query = ()=> Orders.include(p=> [p.customer,p.details.include(p=> p.product.include(p=>p.category))]).page(1,1)
-		const result = await  orm.execute(query,null,options)
-		console.log(JSON.stringify(result))
+		// const query = ()=> Orders.include(p=> [p.customer,p.details.include(p=> p.product.include(p=>p.category))]).page(1,1)
+		// const result = await  orm.execute(query,null,options)
+		// console.log(JSON.stringify(result))
 
-
-
+		const query = (ordNo: number) => Orders.details.filter(o => o.orderId == ordNo).map(o => ({ id: o.orderId, result: avg(o.unitPrice) }))
+		const queryInfo = await orm.getInfo(query)
+		console.log(JSON.stringify(queryInfo))
+		// const result = await orm.execute(query, { ordNo: 5 }, options)
+		// console.log(JSON.stringify(result))
 		
 	} catch (error:any) {
 		console.error(error.stack)
