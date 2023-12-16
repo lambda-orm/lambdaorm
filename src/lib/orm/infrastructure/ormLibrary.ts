3a7cdffca7ae94d5e92afbcc17b2c19eca6d90a6
FlavioLionelRita <flaviolrital@hotmail.com>
2023-12-16 01:39:51 +0100

    fix(library): solve orm.execute async method
    #201

--- src/lib/orm/infrastructure/ormLibrary.ts
+++ src/lib/orm/infrastructure/ormLibrary.ts
@@ -7,41 +7,38 @@ export class OrmLibrary {
 		this.orm.expressions.addFunction('orm.execute(expression:string,data:any,options:any):any', async (expression:string, data:any, options:any) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return await this.orm.execute(expression, data, options)
-				// TODO: solve async execute
-				// https://www.npmjs.com/package/glome-deasync
-				// const execute = deasync((expression:string, data:any, options:any, done:any) => {
-				// this.orm.execute(expression, data, options)
-				// .then((result:any) => done(null, result))
-				// .catch((error:any) => done(error))
-				// })
-				// const result = execute(expression, data, options)
-				// return result
 			}
-		})
+			return null
+		}, { async: true })
 		this.orm.expressions.addFunction('orm.plan(expression:string,options:any):any', (expression:string, options:any) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return this.orm.plan(expression, options)
 			}
+			return null
 		})
 		this.orm.expressions.addFunction('orm.metadata(expression:string):any', (expression:string) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return this.orm.metadata(expression)
 			}
+			return null
 		})
 		this.orm.expressions.addFunction('orm.model(expression:string):any', (expression:string) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return this.orm.model(expression)
 			}
+			return null
 		})
 		this.orm.expressions.addFunction('orm.parameters(expression:string):any', (expression:string) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return this.orm.parameters(expression)
 			}
+			return null
 		})
 		this.orm.expressions.addFunction('orm.constraints(expression:string):any', (expression:string) => {
 			if (expression !== undefined && expression !== null && expression.trim() !== '') {
 				return this.orm.constraints(expression)
 			}
+			return null
 		})
 	}
 }
