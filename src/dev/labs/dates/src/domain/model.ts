d3091fcee159ea28f1f31cae156d6b1e1cec840e
FlavioLionelRita <flaviolrital@hotmail.com>
2024-05-11 12:11:07 +0200

    fix: comment dates lab
    #0

--- src/dev/labs/dates/src/domain/model.ts
+++ src/dev/labs/dates/src/domain/model.ts
@@ -1,19 +1,19 @@
-/* eslint-disable no-use-before-define */
-// THIS FILE IS NOT EDITABLE, IS MANAGED BY LAMBDA ORM
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Queryable, OneToMany, ManyToOne, OneToOne } from 'lambdaorm'
-export class Test {
-	id?: number
-	description?: string
-	testDate?: Date
-	testDateTime?: Date
-	testString?: string
-}
-export interface QryTest {
-	id: number
-	description: string
-	testDate: Date
-	testDateTime: Date
-	testString: string
-}
-export let Tests: Queryable<QryTest>
+// /* eslint-disable no-use-before-define */
+// // THIS FILE IS NOT EDITABLE, IS MANAGED BY LAMBDA ORM
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import { Queryable, OneToMany, ManyToOne, OneToOne } from 'lambdaorm'
+// export class Test {
+// id?: number
+// description?: string
+// testDate?: Date
+// testDateTime?: Date
+// testString?: string
+// }
+// export interface QryTest {
+// id: number
+// description: string
+// testDate: Date
+// testDateTime: Date
+// testString: string
+// }
+// export let Tests: Queryable<QryTest>
