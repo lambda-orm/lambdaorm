f8c82a2cc7a2807ec58a8f880e271d3fde41079e
FlavioLionelRita <flaviolrital@hotmail.com>
2024-05-13 12:10:17 +0200

    fix: bugfix in dates lab
    #0

--- src/dev/labs/dates/src/domain/repositoryTest.ts
+++
@@ -1,8 +0,0 @@
-import { Repository, IOrm } from 'lambdaorm'
-import { Test, QryTest } from './model'
-export class TestRepository extends Repository<Test, QryTest> {
-	constructor (stage?: string, orm?:IOrm) {
-		super('Tests', stage, orm)
-	}
-	// Add your code here
-}
